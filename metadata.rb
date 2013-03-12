a6857b05d8467f8c7ee1e8680de73947d2a23c06
Jim Rosser <jarosser06@gmail.com>
2013-03-12 15:47:01 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'bind'
+maintainer       'Texas A&M'
+maintainer_email 'jarosser06@arch.tamu.edu'
+license          'All rights reserved'
+description      'Installs/Configures bind9'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '13.2.1'
