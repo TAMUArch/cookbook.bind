c5a856d0ff44e7b7cc060db0cd24d83efa606e8e
Jim <jarosser06@gmail.com>
2013-04-06 08:27:31 -0400

    updated change log, added supports entry to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'mit'
 description      'Installs/Configures bind9'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '13.3.0'
+
+supports         'ubuntu'
