ea6e91e55eaf2c3def2d3afd4696b4f18aaaf408
Jim Rosser <jarosser06@gmail.com>
2014-06-25 22:58:32 -0500

    added default zones

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,6 +19,28 @@ template ::File.join(node['bind']['dir'], 'named.conf') do
   notifies :restart, "service[#{node['bind']['service']}]"
 end
 
+template ::File.join(node['bind']['db_dir'], 'named.conf.default-zones') do
+  source 'named.conf.default-zones.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+end
+
+%w(
+  db.0
+  db.127
+  db.255
+  db.local
+  db.root
+).each do |db|
+  cookbook_file ::File.join(node['bind']['db_dir'], db) do
+    source db
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
+end
+
 service node['bind']['service'] do
   action [:enable, :start]
 end
